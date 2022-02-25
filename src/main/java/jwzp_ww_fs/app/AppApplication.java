a00e4379ec19933afe63e14d02410ecfd034aad4
Wojciech Wykret <57493639+WWykret@users.noreply.github.com>
2022-02-25 18:20:18 +0100

    Initial commit

---
+++ src/main/java/jwzp_ww_fs/app/AppApplication.java
@@ -0,0 +1,13 @@
+package jwzp_ww_fs.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppApplication.class, args);
+	}
+
+}
