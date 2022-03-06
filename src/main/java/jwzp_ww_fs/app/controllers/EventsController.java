d9130df3e5ea04597073887e332d2aa14a4a78f3
Wojciech Wykret <57493639+WWykret@users.noreply.github.com>
2022-03-06 12:56:38 +0100

    Changed events mapping

--- src/main/java/jwzp_ww_fs/app/controllers/EventsController.java
+++ src/main/java/jwzp_ww_fs/app/controllers/EventsController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +18,7 @@ import jwzp_ww_fs.app.models.Event;
 import jwzp_ww_fs.app.services.EventsService;
 
 @RestController
+@RequestMapping("/api/events")
 public class EventsController {
 
     private EventsService service;
@@ -26,31 +28,31 @@ public class EventsController {
         this.service = service;
     }
 
-    @PostMapping("/api/events")
-    public Event addEvent(@RequestBody Event event) {
-        return service.addEvent(event);
-    }
-
-    @GetMapping("/api/events/{id}")
-    public Event getEvent(@PathVariable int id) {
-        return service.getEvent(id);
-    }
-
-    @DeleteMapping("/api/events/{id}")
-    public Event deleteEvent(@PathVariable int id) {
-        return service.removeEvent(id);
-    }
-
-    @PatchMapping("/api/events/{id}")
-    public Event updateEvent(@PathVariable int id, @RequestBody Event event) {
-        return service.updateEvent(id, event);
-    }
-
-    @GetMapping("/api/events")
+    @GetMapping("")
     public List<Event> getEventWithCoachAndClub(@RequestParam Optional<Integer> coachId, @RequestParam Optional<Integer> clubId) {
         if (coachId.isEmpty() && clubId.isEmpty()) return service.getAllEvents();
         else if (coachId.isEmpty() && clubId.isPresent()) return service.getEventsByClub(clubId.get());
         else if (coachId.isPresent() && clubId.isEmpty()) return service.getEventsByCoach(coachId.get());
         return service.getEventsByCoachAndClub(coachId.get(), clubId.get());
+    }
+
+    @PostMapping("")
+    public Event addEvent(@RequestBody Event event) {
+        return service.addEvent(event);
+    }
+
+    @GetMapping("/{id}")
+    public Event getEvent(@PathVariable int id) {
+        return service.getEvent(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public Event deleteEvent(@PathVariable int id) {
+        return service.removeEvent(id);
+    }
+
+    @PatchMapping("/{id}")
+    public Event updateEvent(@PathVariable int id, @RequestBody Event event) {
+        return service.updateEvent(id, event);
     }
 }
